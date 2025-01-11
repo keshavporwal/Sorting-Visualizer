32b5c44fadb1eebb0634ff82c466d646748381fb
keshavporwal <58173919+keshavporwal@users.noreply.github.com>
2025-01-11 22:29:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SortingVisualiser from './Components/Sorting-Main/SortingVisualiser';
+
+function App() {
+  return (
+    <div className="App"> 
+        <SortingVisualiser />
+    </div>
+  );
+}
+
+export default App;
